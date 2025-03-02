3aae99b9d0c8ec2d54f5fae03f482cf58a1b487c
pachi1376 <kurokiri1763@gmail.com>
2025-03-03 02:48:49 +0900

    主要機能の追加

---
+++ console-project/Program.cs
@@ -0,0 +1,54 @@
+﻿List<string> tasks = [];
+
+while(true)
+{
+// タスク表示コマンドを読み取る
+    Console.Write("Type command: ");
+    string command = Console.ReadLine();
+    // コマンドによって動作切り替え
+    // 表示コマンド
+    if (command == "display")
+    {
+        // タスクを表示
+        foreach(string task in tasks)
+        {
+            Console.WriteLine(task);
+        }
+    }
+
+    // 追加コマンド
+    else if (command == "add")
+    {
+        // タスクを追加
+        Console.Write("Type your newtask: ");
+        String newTask = Console.ReadLine();
+        tasks.Add(newTask);
+    }
+
+    // 更新コマンド
+    else if (command == "edit")
+    {
+        // タスクを更新
+        Console.Write("Type your edittask: ");
+        String editTask = Console.ReadLine();
+
+        // 既存のタスクを参照する
+        int indexNum = tasks.IndexOf(editTask);
+        Console.Write("Type your edittext: ");
+        String editText = Console.ReadLine();
+        
+        // 参照した値を変更する
+        tasks[indexNum] = editText;
+
+    }
+    // 削除コマンド
+    else if (command == "remove")
+    {
+        Console.Write("Type your removetask: ");
+        String removeTask = Console.ReadLine();
+        tasks.Remove(removeTask);
+        // タスクを削除
+
+    }
+
+}
