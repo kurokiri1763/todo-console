a223ce77dd2f2383081a406ae76be47911d3ad57
pachi1376 <kurokiri1763@gmail.com>
2025-03-03 00:06:38 +0900

    終末

--- App.ts
+++ App.ts
@@ -1,1 +1,19 @@
-console.log("Hello World");
+import * as readline from 'readline';
+
+// readlineインターフェースを作成
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function input() {
+    let result = "";
+    rl.question("お名前は?：　", (answer) => {
+      rl.close();
+      result = answer;
+    })
+    return result;
+  }
+
+const a = input()
+console.log(a)
