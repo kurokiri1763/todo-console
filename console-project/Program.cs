2a0fff55d7aefbc2e52038b3cf09d3dc4e4e1ba0
pachi1376 <kurokiri1763@gmail.com>
2025-03-05 23:11:34 +0900

    関数化

--- console-project/Program.cs
+++ console-project/Program.cs
@@ -1,71 +1,91 @@
 ﻿List<string> tasks = [];
 
+void Display()
+{
+    // タスクを表示
+    foreach(string task in tasks)
+    {
+        Console.WriteLine(task);
+    }
+}
+
+void Add()
+{
+    // タスクを追加
+    Console.Write("Type your newtask: ");
+    String newTask = Console.ReadLine();
+    tasks.Add(newTask);
+}
+
+void Edit()
+{
+// タスクを更新
+    Console.Write("Type your edittask: ");
+    String editTask = Console.ReadLine();
+    // 既存のタスクを参照する
+    int indexNum = tasks.IndexOf(editTask);
+
+    if (indexNum == -1)
+    {
+        Console.WriteLine("Please enter a valid task");
+    }
+
+    else
+    {
+    Console.Write
+    ("Type your newtext: ");
+    String newText = Console.ReadLine();
+    
+    // 参照した値を変更する
+    tasks[indexNum] = newText;
+    }
+}
+
+void Remove()
+{
+    // タスクを削除
+    Console.Write("Type your removetask: ");
+    String removeTask = Console.ReadLine();
+    tasks.Remove(removeTask);
+}
+
+static void CommandError()
+{
+    Console.WriteLine("error");
+    Console.WriteLine("This command does not exist.");
+    Console.WriteLine("Please type a valid command.");
+}
+
 
 while(true)
 {
-// タスク表示コマンドを読み取る
+       // タスク表示コマンドを読み取る
     Console.Write("Type command: ");
     string command = Console.ReadLine();
     // コマンドによって動作切り替え
     // 表示コマンド
     if (command == "display")
     {
-        // タスクを表示
-        foreach(string task in tasks)
-        {
-            Console.WriteLine(task);
-        }
+        Display();
     }
-
     // 追加コマンド
     else if (command == "add")
     {
-        // タスクを追加
-        Console.Write("Type your newtask: ");
-        String newTask = Console.ReadLine();
-        tasks.Add(newTask);
+        Add();
     }
-
     // 更新コマンド
     else if (command == "edit")
     {
-        // タスクを更新
-        Console.Write("Type your edittask: ");
-        String editTask = Console.ReadLine();
-        // 既存のタスクを参照する
-        int indexNum = tasks.IndexOf(editTask);
-
-        if (indexNum == -1)
-        {
-            Console.WriteLine("Please enter a valid task");
-        }
-
-        else
-        {
-        Console.Write
-        ("Type your newtext: ");
-        String newText = Console.ReadLine();
-        
-        // 参照した値を変更する
-        tasks[indexNum] = newText;
-        }
-
+        Edit();
     }
     // 削除コマンド
     else if (command == "remove")
     {
-        // タスクを削除
-        Console.Write("Type your removetask: ");
-        String removeTask = Console.ReadLine();
-        tasks.Remove(removeTask);
-
+        Remove();
     }
 
     else
     {
-        Console.WriteLine("error");
-        Console.WriteLine("This command does not exist.");
-        Console.WriteLine("Please type a valid command.");
+        CommandError();
     }
-
 }
